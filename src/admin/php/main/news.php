79e3a8775386abcfa629f78dcd973802b4fab289
yuya0715 <negochan87@gmail.com>
2021-05-29 14:16:40 +0900

    news.php修正中

--- src/admin/php/main/news.php
+++ src/admin/php/main/news.php
@@ -2,11 +2,11 @@
 include("../parts/escape.php");
 include("../parts/db.php");
 
+
 $newsId = filter_input(INPUT_POST, 'newsId');
 $status = filter_input(INPUT_POST, 'newsStatus');
 
-
-// 更新
+// 編集画面に遷移
 if (!empty($newsId) && $status === 'update') {
 
   try {
@@ -18,63 +18,65 @@ if (!empty($newsId) && $status === 'update') {
   } catch(PDOException $e) {
     exit($e);
   }
-
-  var_dump($newsId);
-  var_dump($status);
   $postDate = $row5['news_day'];
   $postTitle = $row5['news_title'];
   $postImage = $row5['news_image'];
   $postText = $row5['news_text'];
   $timestamp = date("YmdHi");
-  var_dump($postDate);
-  var_dump($postTitle);
-  var_dump($postImage);
-  var_dump($postText);
-  var_dump($timestamp);
+
+}
+
 
   // 投稿 or 保存
-} elseif ($status === 'post' || $status === 'save') {
-  $postDate = filter_input(INPUT_POST, 'postDate');
-  $postTitle = filter_input(INPUT_POST, 'postTitle');
-  $postImage = filter_input(INPUT_POST, 'postImage');
-  $postText = filter_input(INPUT_POST, 'postText');
-  $timestamp = date("YmdHi");
+ if ($status === 'post' || $status === 'save') {
+     $postDate = filter_input(INPUT_POST, 'postDate');
+     $postTitle = filter_input(INPUT_POST, 'postTitle');
+     $postImage = filter_input(INPUT_POST, 'postImage');
+     $postText = filter_input(INPUT_POST, 'postText');
+     $timestamp = date("YmdHi");
+     var_dump($newsId);
+     var_dump($status);
+     var_dump($postDate);
+     var_dump($postTitle);
+     var_dump($postImage);
+     var_dump($postText);
+     var_dump($timestamp);
 
-  // insert
-  if (empty($newsId)) {
-    $newsId = strval(date("YmdHi")) . strval(mt_rand(10, 99));
+     // insert
+     if (empty($newsId)) {
+         $newsId = strval(date("YmdHi")) . strval(mt_rand(10, 99));
 
-    try {
-      $stmt = $dbh->prepare("
+         try {
+             $stmt = $dbh->prepare("
           INSERT INTO
             news_table(news_id,news_day,news_title,news_text,news_image,news_status,news_timestamp)
           VALUES
             (:news_id,:news_day,:news_title,:news_text,:news_image,:news_status,:news_timestamp)
         ");
 
-      $stmt->bindValue(':news_id', $newsId, PDO::PARAM_STR);
-      $stmt->bindValue(':news_day', $postDate, PDO::PARAM_STR);
-      $stmt->bindValue(':news_title', $postTitle, PDO::PARAM_STR);
-      $stmt->bindValue(':news_text', $postText, PDO::PARAM_STR);
-      $stmt->bindValue(':news_image', $postImage, PDO::PARAM_STR);
-      $stmt->bindValue(':news_status', $status, PDO::PARAM_STR);
-      $stmt->bindValue(':news_timestamp', $timestamp, PDO::PARAM_STR);
-      $stmt->execute();
+             $stmt->bindValue(':news_id', $newsId, PDO::PARAM_STR);
+             $stmt->bindValue(':news_day', $postDate, PDO::PARAM_STR);
+             $stmt->bindValue(':news_title', $postTitle, PDO::PARAM_STR);
+             $stmt->bindValue(':news_text', $postText, PDO::PARAM_STR);
+             $stmt->bindValue(':news_image', $postImage, PDO::PARAM_STR);
+             $stmt->bindValue(':news_status', $status, PDO::PARAM_STR);
+             $stmt->bindValue(':news_timestamp', $timestamp, PDO::PARAM_STR);
+             $stmt->execute();
 
-      if ($status === 'post') {
-        echo "<script>alert('投稿しました');</script>";
-      } elseif ($status === 'save') {
-        echo "<script>alert('保存しました');</script>";
-      }
-      $status = '';
-    } catch (PDOException $e) {
-      exit($e);
-    }
+             if ($status === 'post') {
+                 echo "<script>alert('投稿しました');</script>";
+             } elseif ($status === 'save') {
+                 echo "<script>alert('保存しました');</script>";
+             }
+             $status = '';
+         } catch (PDOException $e) {
+             exit($e);
+         }
 
-    // update
-  } else {
-    try {
-      $stmt2 = $dbh->prepare("
+         // update
+     } else {
+         try {
+             $stmt2 = $dbh->prepare("
           UPDATE
             news_table
           SET
@@ -87,38 +89,43 @@ if (!empty($newsId) && $status === 'update') {
             news_id = :news_id
         ");
 
-      $stmt2->bindValue(':news_id', $newsId, PDO::PARAM_STR);
-      $stmt2->bindValue(':news_day', $postDate, PDO::PARAM_STR);
-      $stmt2->bindValue(':news_title', $postTitle, PDO::PARAM_STR);
-      $stmt2->bindValue(':news_text', $postText, PDO::PARAM_STR);
-      $stmt2->bindValue(':news_image', $postImage, PDO::PARAM_STR);
-      $stmt2->bindValue(':news_status', $status, PDO::PARAM_STR);
-      $stmt2->bindValue(':news_timestamp', $timestamp, PDO::PARAM_STR);
-      $stmt2->execute();
+             $stmt2->bindValue(':news_id', $newsId, PDO::PARAM_STR);
+             $stmt2->bindValue(':news_day', $postDate, PDO::PARAM_STR);
+             $stmt2->bindValue(':news_title', $postTitle, PDO::PARAM_STR);
+             $stmt2->bindValue(':news_text', $postText, PDO::PARAM_STR);
+             $stmt2->bindValue(':news_image', $postImage, PDO::PARAM_STR);
+             $stmt2->bindValue(':news_status', $status, PDO::PARAM_STR);
+             $stmt2->bindValue(':news_timestamp', $timestamp, PDO::PARAM_STR);
+             $stmt2->execute();
 
-      if ($status === 'post') {
-        echo "<script>alert('更新して投稿しました');</script>";
-      } elseif ($status === 'save') {
-        echo "<script>alert('更新して保存しました');</script>";
-      }
-      $status = '';
-    } catch (PDOException $e) {
-      exit($e);
-    }
-  }
-
-  // 削除
-} elseif ($status === 'delete') {
-  try {
-    $stmt3 = $dbh->prepare("DELETE FROM news_table WHERE news_id = :news_id");
-    $stmt3->bindValue(':news_id', $newsId, PDO::PARAM_STR);
-    $stmt3->execute();
-    echo  "<script>alert('削除が完了しました');</script>";
-    $status = '';
-  } catch (PDOException $e) {
-    exit($e);
-  }
+             if ($status === 'post') {
+                 echo "<script>alert('更新して投稿しました');</script>";
+             } elseif ($status === 'save') {
+                 echo "<script>alert('更新して保存しました');</script>";
+             }
+             $status = '';
+         } catch (PDOException $e) {
+             exit($e);
+         }
+     }
 }
+
+
+
+//   // 削除
+//  } elseif ($status === 'delete') {
+//   try {
+//     $stmt3 = $dbh->prepare("DELETE FROM news_table WHERE news_id = :news_id");
+//     $stmt3->bindValue(':news_id', $newsId, PDO::PARAM_STR);
+//     $stmt3->execute();
+//     echo  "<script>alert('削除が完了しました');</script>";
+//     $status = '';
+//   } catch (PDOException $e) {
+//     exit($e);
+//   }
+// }
+
+
 
 include("../parts/header.php");
 include("../parts/sidebar.php");
@@ -148,6 +155,8 @@ include("../parts/sidebar.php");
       <div class="row">
         <form action="" method="post">
           <button type="submit" name="newsStatus" value="new">新規記事作成</button>
+        </form>
+        <form action="" method="post">
           <table class="table table-hover">
             <thead>
               <tr>
@@ -193,12 +202,12 @@ include("../parts/sidebar.php");
           <!-- 投稿日時 -->
           <div class="form-group col-5 mb-3">
             <label for="postDate">投稿日時</label>
-            <input class="form-control" id="postDate" type="datetime-local" name="postDate" value="<?php echo h($postDate); ?>" required>
+            <input class="form-control" id="postDate" type="date" name="postDate" >
           </div>
           <!-- タイトル -->
           <div class="form-group col-5 mb-3">
             <label for="postTitle">タイトル</label>
-            <input id="postTitle" class="form-control" name="postTitle" value="<?php echo h($postTitle); ?>" required>
+            <input id="postTitle" class="form-control" name="postTitle" value="<?php echo h($postTitle); ?>" >
           </div>
           <!-- 画像 -->
           <div class="form-group col-5 mb-3">
@@ -211,14 +220,16 @@ include("../parts/sidebar.php");
           <!-- 内容 -->
           <div class="form-group col-8 mb-3">
             <label for="postText">内容</label>
-            <textarea id="postText" class="form-control" name="postText" rows="6" cols="40" required><?php echo h($postText); ?></textarea>
+            <textarea id="postText" class="form-control" name="postText" rows="6" cols="40" ><?php echo h($postText); ?></textarea>
           </div>
         </div>
         <div class="col-5 mb-3">
-          <button class="btn btn-outline-primary mr-2" type="submit" name="newsStatus" value="post">投稿</button>
-          <button class="btn btn-outline-success mr-2" type="submit" name="newsStatus" value="save">保存</button>
-          <button class="btn btn-outline-secondary mr-2" type="submit" name="newsStatus" value="back">戻る</button>
-          <?php if ($status === 'update') : ?>
+        <!-- 編集の時のボタン -->
+
+            <button class="btn btn-outline-primary mr-2" type="submit" name="newsStatus" value="post">投稿</button>
+            <button class="btn btn-outline-success mr-2" type="submit" name="newsStatus" value="save">保存</button>
+            <button class="btn btn-outline-secondary mr-2" type="submit" name="newsStatus" value="back">戻る</button>
+            <?php if ($status === 'update') : ?>
             <button class="btn btn-outline-danger mr-2" type="submit" name="newsStatus" value="delete" onClick="return window.confirm('本当に削除してよろしいですか？');">削除
               <input type="hidden" name="newsId" value="<?php echo h($newsId); ?>">
             </button>
