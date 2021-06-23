646ecab559abf079b6d7b4b4bcb806f2a8025acc
yuya0715 <negochan87@gmail.com>
2021-06-23 20:52:03 +0900

    ご指摘いただいところを修正しました。 再度、ご確認お願いします。

--- src/admin/php/parts/sidebar.php
+++ src/admin/php/parts/sidebar.php
@@ -121,8 +121,8 @@
               </a>
             </li>
             <li class="nav-item">
-              <a href="/src/admin/php/brand/index.php" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
+              <a href="/src/admin/php/main/brand.php" class="nav-link active">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>Brand</p>
               </a>
             </li>
@@ -136,12 +136,6 @@
               <a href="/src/admin/php/dropimg/img.php" class="nav-link active">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>画像一覧</p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="/src/admin/php/main/shop.php" class="nav-link active">
-                <i class="nav-icon fas fa-tachometer-alt"></i>
-                <p>Interview</p>
               </a>
             </li>
             <li class="nav-item">
